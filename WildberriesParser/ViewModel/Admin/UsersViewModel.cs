f29c63882708e3942845b2f240e80ed1c7865b94
mimo113 <mimo113dev@mail.ru>
2023-04-02 15:35:46 +0300

    Remove setters of FontFamily = "Lato"

--- WildberriesParser/ViewModel/Admin/UsersViewModel.cs
+++ WildberriesParser/ViewModel/Admin/UsersViewModel.cs
@@ -1,8 +1,33 @@
-﻿using WildberriesParser.Infastructure.Core;
+﻿using System.Collections.Generic;
+using WildberriesParser.Infastructure.Core;
+using WildberriesParser.Model.Data;
+using WildberriesParser.Services;
+using System.Linq;
 
 namespace WildberriesParser.ViewModel.Admin
 {
     public class UsersViewModel : ViewModelBase
     {
+        private List<User> _users;
+        private INavigationService _navigationService;
+
+        public List<User> Users
+        {
+            get => _users;
+            set => Set(ref _users, value);
+        }
+
+        public INavigationService NavigationService
+        {
+            get => _navigationService;
+            set => Set(ref _navigationService, value);
+        }
+
+        public UsersViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+
+            _users = DBEntities.GetContext().User.ToList();
+        }
     }
 }
